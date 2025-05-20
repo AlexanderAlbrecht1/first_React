1ef1b201b7bde912f0fe4dddbaa8018082ee8618
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-05-21 00:03:41 +0200

    first product

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Navbar from './components/navbar';
+import Product from './components/product';
 import "bootstrap/dist/css/bootstrap.min.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Navbar/>);
+root.render(<Product/>);
